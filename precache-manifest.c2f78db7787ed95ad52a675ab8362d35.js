973763d1294a12a6e3fa72ce4726a0e9505a1f81
fillerInk <thatonephysicsnerd@gmail.com>
2020-07-16 17:34:41 +0530

    Updates

---
+++ precache-manifest.c2f78db7787ed95ad52a675ab8362d35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5baa424f19400f3609ef999dec3b88",
+    "url": "/readit/index.html"
+  },
+  {
+    "revision": "26e42ec104d618136a4f",
+    "url": "/readit/static/css/main.e01b1d05.chunk.css"
+  },
+  {
+    "revision": "43e7aba553f99859fadb",
+    "url": "/readit/static/js/2.f9e83037.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/readit/static/js/2.f9e83037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e42ec104d618136a4f",
+    "url": "/readit/static/js/main.48e48465.chunk.js"
+  },
+  {
+    "revision": "30055b9f8c59599f7fe1",
+    "url": "/readit/static/js/runtime-main.7d85d9b1.js"
+  }
+]);
