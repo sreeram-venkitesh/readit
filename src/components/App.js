7d6b928bb4fadbe3971d524337f2d3863df8e3c6
fillerInk <thatonephysicsnerd@gmail.com>
2020-07-16 16:09:00 +0530

    Fetching posts works

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/App.css';
 
 function App() {
+  const [subreddit,setSubreddit] = useState('all')
+
+  // const select = document.getElementById('optionSelect').value
+  // console.log(select)
+
+
   return (
     <div className="App">
       <h1>readit</h1>
+      <p className='para'>
+      readit is a simple app written in React to help you read good posts from reddit.
+      You can select any other subreddit you want
+      to read from here.
+      </p>
+      
     </div>
   );
 }
